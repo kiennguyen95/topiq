68474915ecd7e1edb4424a136629f4041171f06d
Kien Nguyen <kien.nguyentrung.95@gmail.com>
2019-09-14 15:50:47 +0700

    Style profile page

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -5,60 +5,56 @@
 @endsection
 @section('content')
   <div class="profile-wrapper">
-    <div class="profile-top-bar">
-    </div>
-    <div class="profile-content">
-      <div class="avatar-wrapper">
-        <div class="avatar"><img src="{{ $user->avatar_src }}" alt=""></div>
-        <div class="name">{{ $user->name }}</div>
-      </div>
+    <div class="user-info">
+      <div class="avatar"><img src="{{ $user->avatar_src }}" alt=""></div>
+      <div class="name">{{ $user->name }}</div>
       <div class="account-balance">
         <div class="value">{{ $user->balance }} VNĐ</div>
         <div class="label">Số tiền chiến thắng</div>
       </div>
+    </div>
 
-      <div class="games-statistics owl-carousel">
-        @foreach ($user->games as $game)
-          <div class="game-achievements">
-            <div class="game-icon"><img src="{{ $game->icon_src }}" alt="{{ $game->icon_alt }}"></div>
-            <div class="info">
-              <ul>
-                <li>
-                  <span>{{ $game->name }}</span>
-                  <span>Hạng {{ $game->rank }}</span>
-                </li>
-                <li>
-                  <span>Elo</span>
-                  <span>{{ $game->elo }}</span>
-                </li>
-                <li>
-                  <span>Thắng</span>
-                  <span>{{ $game->won }}</span>
-                </li>
-                <li>
-                  <span>Thua</span>
-                  <span>{{ $game->lost }}</span>
-                </li>
-                <li>
-                  <span>Hòa</span>
-                  <span>{{ $game->draw }}</span>
-                </li>
-              </ul>
-              <ul class="medal-list">
-                <li class="medal-gold">
-                  <span>{{ $game->medal_gold }}</span>
-                </li>
-                <li class="medal-silver">
-                  <span>{{ $game->medal_silver }}</span>
-                </li>
-                <li class="medal-bronze">
-                  <span>{{ $game->medal_bronze }}</span>
-                </li>
-              </ul>
-            </div>
+    <div class="games-statistics owl-carousel">
+      @foreach ($user->games as $game)
+        <div class="game-achievements">
+          <div class="game-icon"><img src="{{ $game->icon_src }}" alt="{{ $game->icon_alt }}"></div>
+          <div class="info">
+            <ul>
+              <li>
+                <span>{{ $game->name }}</span>
+                <span>Hạng {{ $game->rank }}</span>
+              </li>
+              <li>
+                <span>Elo</span>
+                <span>{{ $game->elo }}</span>
+              </li>
+              <li>
+                <span>Thắng</span>
+                <span>{{ $game->won }}</span>
+              </li>
+              <li>
+                <span>Thua</span>
+                <span>{{ $game->lost }}</span>
+              </li>
+              <li>
+                <span>Hòa</span>
+                <span>{{ $game->draw }}</span>
+              </li>
+            </ul>
+            <ul class="medal-list">
+              <li class="medal-gold">
+                <span>{{ $game->medal_gold }}</span>
+              </li>
+              <li class="medal-silver">
+                <span>{{ $game->medal_silver }}</span>
+              </li>
+              <li class="medal-bronze">
+                <span>{{ $game->medal_bronze }}</span>
+              </li>
+            </ul>
           </div>
-        @endforeach
-      </div>
+        </div>
+      @endforeach
     </div>
   </div>
 @endsection
