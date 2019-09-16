4c6771b88bc0df97d3c77330c0b6df173783d113
Kien Nguyen <kien.nguyentrung.95@gmail.com>
2019-09-17 00:12:56 +0700

    Style profile page PC

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,9 @@
 <div id="footer">
   <ul class="links-wrapper">
-    <li class="link-fb">Facebook.com/TOPIQapp</li>
-    <li class="link-ios">Tải game trên IOS</li>
-    <li class="link-android">Tải game trên Android</li>
-    <li class="link-hotline">Hotline: 19007 17171</li>
+    <li class="link-fb"><a href="https://www.facebook.com/TOPIQapp/">Facebook.com/TOPIQapp</a></li>
+    <li class="link-ios"><a href="https://apps.apple.com/vn/app/topiq-gi%E1%BA%A3i-th%C6%B0%E1%BB%9Fng-ti%E1%BB%81n-m%E1%BA%B7t/id1412605160">Tải game trên IOS</a></li>
+    <li class="link-android"><a href="https://play.google.com/store/apps/details?id=com.vtechnology.topiq">Tải game trên Android</a></li>
+    <li class="link-hotline"><a href="#">Hotline: 19007 17171</a></li>
   </ul>
   <div class="copyright">Copyright © 2019 TOPIQ. All rights reserved.</div>
 </div>
