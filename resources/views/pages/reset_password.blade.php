db2885da85d99c41215d680f3ee3bbb6d585a1b7
Nguyễn Hữu Toàn - CMC Global DU2 <nhtoan1@LDAP.CMCGLOBAL.COM.VN>
2019-09-17 18:06:51 +0700

    create change password page

---
+++ resources/views/pages/reset_password.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('style')
+@parent
+<link rel="stylesheet" type="text/css" href="/css/reset_pass.css">
+@endsection
+@section('content')
+<div class="reset-pass-wrapper">
+    <div class="reset-pass-content">
+        <p id="reset-pass-title">Đổi mật khẩu cho tài khoản của bạn</p>
+        <form id="reset-pass-form" action="/action_page.php">
+            <label for="password">Mật khẩu</label>
+            <input type="password" id="password" name="password" placeholder="Nhập mật khẩu">
+
+            <label for="confirm-password">Nhập lại mật khẩu</label>
+            <input type="password" id="confirm-password" name="confirm-password" placeholder="Nhập mật khẩu">
+            <br>
+            <input type="submit" value="Xong">
+        </form>
+    </div>
+</div>
+@endsection
